01697239a81e33f2c8b493e614dde2cf8a16f2e1
quoccuong.nguyen265@hotmail.com <quoccuong.nguyen265@hotmail.com>
2018-04-22 23:42:51 +0700

    Demo Slider for Brands' Logo & Product Layout

---
+++ Final-Project_Backpack-And-Handbag/source/assets/js/effect.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#recipeCarousel').carousel({
+        interval: 10000
+      })
+      
+      $('.carousel .carousel-item').each(function(){
+          var next = $(this).next();
+          if (!next.length) {
+          next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+          
+          for (var i=0;i<2;i++) { //display 4 item at one
+              next=next.next();
+              if (!next.length) {
+                  next = $(this).siblings(':first');
+                }
+              
+              next.children(':first-child').clone().appendTo($(this));
+            }
+      });
+      
+});
