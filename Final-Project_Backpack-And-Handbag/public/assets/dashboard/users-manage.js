ccb56079e9f0abd102dbfb591ec4c12e30925f31
Minh Nhat <mocking.key118@gmail.com>
2018-05-09 22:39:33 +0700

    recreate...

---
+++ Final-Project_Backpack-And-Handbag/public/assets/dashboard/users-manage.js
@@ -0,0 +1,32 @@
+var i = 2;
+
+$('#btnAdd').on('click', function () {
+    var newRow = `
+		<tr style="display: none">
+			<td>` + i + `</td>
+            <td>Trần Văn A</td>
+            <td>Quận 1, TP HCM</td>
+            <td>vana@gmail.com</td>
+            <td>234290489</td>
+            <td>1 000 000</td>
+			<td>
+				<a class="update btn btn-xs btn-danger" href="javascript:;" role="button">
+					<span class="fa fa-wrench"></span>
+                </a>
+                <a class="remove btn btn-xs btn-success" href="javascript:;" role="button">
+					<span class="fa fa-window-close"></span>
+				</a>
+			</td>
+		</tr>`;
+    i++;
+    $('#list').append(newRow);
+    $('#list tr:last').show(1000);
+});
+
+$('#list').on('click', 'a.remove', function () {
+    $(this).closest('tr').remove();
+});
+
+$('#list').on('click', 'a.update', function () {
+
+});
---
+++ Final-Project_Backpack-And-Handbag/public/assets/js/product-details.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $('#overviewSlide').carousel({
+        interval: 5000
+    })
+    $('#recipeCarousel').carousel({
+        interval: 5000
+      })
+      
+    $('.sameTypeProducts .carousel .carousel-item').each(function(){ 
+        var next = $(this).next();
+        if (!next.length) {
+        next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<3;i++) { //display 4 item at one
+            next=next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+    $('.sameVendorProducts .carousel .carousel-item').each(function(){ 
+        var next = $(this).next();
+        if (!next.length) {
+        next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<3;i++) { //display 4 item at one
+            next=next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+    $('#signupModal').modal({backdrop: 'static', keyboard: false});
+});
