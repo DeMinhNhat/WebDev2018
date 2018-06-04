894fa069542621d5bce0ac381a6a9b42110ef78f
Minh Nhat <mocking.key118@gmail.com>
2018-06-05 00:33:13 +0700

    create directories for project

---
+++ Final-Project_Backpack-And-Handbag/source/repos/brandRepo.js
@@ -0,0 +1,17 @@
+var db = require('./database');
+var config = require('../config/config');
+
+exports.loadAll = () => {
+	var sql = 'select * from brands';
+	return db.load(sql);
+}
+
+exports.single = id => {
+	var sql = `select * from brands where BraID = ${id}`;
+	return db.load(sql);
+}
+
+exports.delete = id => {
+	var sql = `delete from brands where BraID = ${id}`;
+	return db.save(sql);
+}
---
+++ Final-Project_Backpack-And-Handbag/source/repos/categoryRepo.js
@@ -0,0 +1,17 @@
+var db = require('./database');
+var config = require('../config/config');
+
+exports.loadAll = () => {
+	var sql = 'select * from categories';
+	return db.load(sql);
+}
+
+exports.single = id => {
+	var sql = `select * from categories where CatID = ${id}`;
+	return db.load(sql);
+}
+
+exports.delete = id => {
+	var sql = `delete from categories where CatID = ${id}`;
+	return db.save(sql);
+}
