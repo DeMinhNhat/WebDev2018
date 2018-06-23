899faa425ec03ed1ef2b6f29895a4c4d2cc15520
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-06-23 15:04:42 +0700

    Add adapting productView's height while resize window

--- Final-Project_Backpack-And-Handbag/source/public/assets/home/js/products.js
+++ Final-Project_Backpack-And-Handbag/source/public/assets/home/js/products.js
@@ -14,4 +14,30 @@ $(document).ready(function () {
             ) * count
         );
     }
+    //Adapting height of #productView
+    $(window).on('resize',function(){
+        var p = $('#productView');
+        console.log($(window).width())
+        var win = $(window);
+        if(bp_items.length > 0){
+            console.log(bp_items.length)
+            var firstChild = bp_items.first();
+            if(win.width()< 1200){
+                p.css('height',  (
+                    parseInt(firstChild.css('height')) 
+                    + parseInt(firstChild.css('margin-bottom'))*2
+                    ) * (count+1)
+                );
+            }
+            else{
+                p.css('height',  (
+                    parseInt(firstChild.css('height')) 
+                    + parseInt(firstChild.css('margin-bottom'))*2
+                    ) * (count)
+                );
+            }
+        }
+        
+    })
+    
 });
