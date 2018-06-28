9e98352d9fbf472123268e1fb366826c1bfef679
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-06-29 02:31:53 +0700

    modify cart.js , fix bug add/sub by 2

--- Final-Project_Backpack-And-Handbag/source/Public/assets/home/js/cart.js
+++ Final-Project_Backpack-And-Handbag/source/Public/assets/home/js/cart.js
@@ -5,9 +5,6 @@ $('.removeItem').on('click', function() {
 });
 $('.addByOne').on('click', function() {
 	var quantity = $('#numItems');
-	if(quantity && parseInt(quantity[0].value) < 10){
-		quantity[0].value = parseInt(quantity[0].value) + 1;
-	}
 	var id = $(this).data('proid');
 	$('#frmGetAmount #txtP').val(id);
 	$('#txtQ').val(quantity[0].value);
@@ -15,9 +12,6 @@ $('.addByOne').on('click', function() {
 });
 $('.subByOne').on('click', function() {
 	var quantity = $('#numItems');
-	if(quantity && parseInt(quantity[0].value) < 10){
-		quantity[0].value = parseInt(quantity[0].value) - 1;
-	}
 	var id = $(this).data('proid');
 	$('#frmGetAmount #txtP').val(id);
 	$('#txtQ').val(quantity[0].value);
