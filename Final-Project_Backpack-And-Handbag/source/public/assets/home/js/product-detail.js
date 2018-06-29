59063a71ba9b31e0d4535bed4405002de0c35b2c
ApolloRisky <quoccuong.nguyen265@hotmail.com>
2018-06-29 07:53:02 +0700

    DEBUG product-detail.js

--- Final-Project_Backpack-And-Handbag/source/public/assets/home/js/product-detail.js
+++ Final-Project_Backpack-And-Handbag/source/public/assets/home/js/product-detail.js
@@ -9,14 +9,20 @@ $(document).ready(function() {
     $('#subByOne').on('click', function() {
         var quantity = $('#numItems');
         if (quantity && parseInt(quantity[0].value) > 1) {
+            console.log('before: sub' + quantity[0].value);
             quantity[0].value = parseInt(quantity[0].value) - 1;
+            console.log('after sub: ' + quantity[0].value);
         }
     });
     
     $('#addByOne').on('click', function() {
         var quantity = $('#numItems');
         if (quantity && parseInt(quantity[0].value) < 10) {
+
+            console.log('before: add' + quantity[0].value);
             quantity[0].value = parseInt(quantity[0].value) + 1;
+
+            console.log('after: add' + quantity[0].value);
         }
     })
 });
